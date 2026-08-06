20d9853c7eff3956d16e05b9c80682fa6f51fccd
Mayita Denenga <mayitadenenga@gmail.com>
2026-08-06 18:35:33 +1200

    modified code for q 4.6

--- Lab2_UART/Lab2_Q4/Lab2_Q4/main.c
+++ Lab2_UART/Lab2_Q4/Lab2_Q4/main.c
@@ -48,11 +48,39 @@ void usart_transmit(uint8_t data)
 	UDR0 = data;
 }
 
+void usart_transmit_number(uint16_t number)
+{
+	uint8_t digits[5];   // uint16_t max is 65535, so 5 digits is enough
+	uint8_t digit_count = 0;
+	
+	// Handle 0 as a special case (loop below would otherwise skip it)
+	if (number == 0)
+	{
+		usart_transmit(48);
+		return;
+	}
+	
+	// Extract digits, least-significant first
+	while (number > 0)
+	{
+		digits[digit_count] = number % 10;
+		number = number / 10;
+		digit_count++;
+	}
+	
+	// Transmit in reverse (most-significant first)
+	for (int8_t i = digit_count - 1; i >= 0; i--)
+	{
+		usart_transmit(digits[i] + 48);
+	}
+}
+
 
 int main(void)
 {
 	// initialization
 	usart_init(12);
+	uint16_t number = 345;
 	
 	uint16_t count = 1;
 	uint16_t myPrimes[63] = {1};
@@ -67,9 +95,10 @@ int main(void)
 	
     while (1) 
     {
-		// sends character "3" every 0.5s
+		// sends character "345" every 0.5s
 		_delay_ms(500);
-		usart_transmit(51);
+		usart_transmit_number(number);
+		usart_transmit(32); // space
     }
 }
 
