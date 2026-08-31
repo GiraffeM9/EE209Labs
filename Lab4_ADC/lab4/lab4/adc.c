dc296192d17545a7d7e5bf1f3bf5de29f71bc14b
Mayita Denenga <mayitadenenga@gmail.com>
2026-08-31 14:47:32 +1200

    Completed part 3 of lab 4

--- Lab4_ADC/lab4/lab4/adc.c
+++ Lab4_ADC/lab4/lab4/adc.c
@@ -9,11 +9,22 @@
 #include "adc.h"
 
 void adc_init(){
-	ADMUX = 0b01000010;
-	ADCSRA = 0b00101100;
-	ADCSRB = 0b01000001;
+	ADMUX |= (1 << REFS0);
+	ADCSRA |= (1 << ADEN) | (1 << ADPS2);
 }
 
 uint16_t adc_read(uint8_t channel){
+	ADMUX &= 0xF0; // Clear channel selection (mux bits)
+	ADMUX |= channel; // Set the channel to convert
+	ADCSRA |= (1 << ADIF);   // clear any stale flag
+	ADCSRA |= (1 << ADSC); // Starting an ADC conversion
 	
+	while ((ADCSRA & (1 << ADIF)) == 0) { // ADIF bit is checked to see if it is 0
+		; // If ADIF bit is not 1, wait until it becomes 1 (polls)
+	}
+	
+	uint8_t low = ADCL;   // Important: Must read this first, "freezes" the ADCL/ADCH pair
+	uint8_t high = ADCH;  // Reading this "releases" the register pair, allowing for the next conversion
+	
+	return (high << 8) | low; // Returns ADC value
 }
